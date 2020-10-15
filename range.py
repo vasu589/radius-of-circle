52ac8dbb97e6949ab625dfcdb4ddba1fd65dbc26
vasu589 <72617559+vasu589@users.noreply.github.com>
2020-10-15 15:03:36 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,29 @@
+#python program to print positive numbers in a list using for loop
+
+#list of numbers in list1
+list1=[12,-7,5,64,-14]
+
+
+#iterating each number in list:
+ for num in list1:
+ 
+   #checking condition
+    if num >= 0:
+      print(num, end = " ")
+      
+ #list of numbers in list2    
+list2=[12,14,-95,3]
+
+#itrating each number in list:
+ for num in list2:
+ 
+ #checking condition
+   if num >= 0:
+    print(num,end=" ")
+    
+    
+ output:list1[12,-7,5,64,-14
+        positive numbers are:12,5,64
+        
+        list2[12,14,-95,3]
+        positive numbers are:12,14,3
